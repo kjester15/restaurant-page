5871ba20b977d8cf2103c2b0edb82959195e4fa0
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-08-08 15:18:39 -0700

    Update name and paragraph contents

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import './style.css';
 
 function body() {
   const title = document.createElement('h1')
-  title.innerHTML = "Kyle's Amazing Restaurant";
+  title.innerHTML = "Byte Bistro";
   document.getElementById("content").appendChild(title);
 
   const text = document.createElement('p')
-  text.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa hic, eius minus rem sapiente obcaecati nulla!";
+  text.innerHTML = "At Byte Bistro, we’ve redefined dining with our JavaScript-inspired menu, featuring dishes that are as dynamic and versatile as the code itself. Whether you're a tech enthusiast or simply hungry for something extraordinary, Byte Bistro is the place where every meal is a delightful piece of culinary code.";
   document.getElementById("content").appendChild(text);
 
   const image = document.createElement('img')
