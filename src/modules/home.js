c02efdc4603227d0251a97a53b29e16572218ec4
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-08-08 15:41:08 -0700

    Move home into separate module

---
+++ src/modules/home.js
@@ -0,0 +1,16 @@
+import Restaurant from '../image.jpg';
+
+export function home() {
+  const title = document.createElement('h1')
+  title.innerHTML = "Byte Bistro";
+  document.getElementById("content").appendChild(title);
+
+  const text = document.createElement('p')
+  text.innerHTML = "At Byte Bistro, we’ve redefined dining with our JavaScript-inspired menu, featuring dishes that are as dynamic and versatile as the code itself. Whether you're a tech enthusiast or simply hungry for something extraordinary, Byte Bistro is the place where every meal is a delightful piece of culinary code.";
+  document.getElementById("content").appendChild(text);
+
+  const image = document.createElement('img')
+  image.src = Restaurant;
+  image.classList.add('image');
+  document.getElementById("content").appendChild(image);
+}
