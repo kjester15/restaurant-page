bf2f8d71f996083f30d910e416458a922b601b4a
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-08-08 15:08:28 -0700

    Install css loaders and pull in image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('hello');
+import Restaurant from './image.jpg';
+import './style.css';
+
+function body() {
+  const title = document.createElement('h1')
+  title.innerHTML = "Kyle's Amazing Restaurant";
+  document.getElementById("content").appendChild(title);
+
+  const text = document.createElement('p')
+  text.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa hic, eius minus rem sapiente obcaecati nulla!";
+  document.getElementById("content").appendChild(text);
+
+  const image = document.createElement('img')
+  image.src = Restaurant;
+  image.classList.add('image');
+  document.getElementById("content").appendChild(image);
+}
+
+// document.getElementById("content").appendChild(component());
+body();
